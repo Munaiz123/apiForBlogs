5765df3eb572ea203b446b3153bbd212a49a0c9d
Munaiz <munaizahmed2@gmail.com>
2022-03-23 22:11:51 -0500

    refactoring

--- api/munaiz.js
+++ api/munaiz.js
@@ -5,10 +5,11 @@ const axios = require("axios");
 const app = express.Router();
 
 let articles = [];
+let blog = `https://munaizahmed.com`;
 
 app.get("/:category", (req, res) => {
   axios
-    .get(`https://munaizahmed.com/category/${req.params.category}`)
+    .get(`${blog}/category/${req.params.category}`)
     .then((response) => {
       let html = response.data;
       let $ = cheerio.load(html); // will let us pick out elements
